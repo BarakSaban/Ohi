ddb116b05ced9ed2e93ceab7b8d8b5d5474e1cf5
BarakSaban <59569912+BarakSaban@users.noreply.github.com>
2020-02-03 18:05:05 +0200

    Update ScratchReletiveMovementV2.py

--- ScratchReletiveMovementV2.py
+++ ScratchReletiveMovementV2.py
@@ -3,3 +3,5 @@ import time
 pyautogui.click(1334, 1054)
 pyautogui.click(1000, 500) 
 pyautogui.press('5') 
+#Here I tried making the code function in a different way (using the keyboard primarily) to make it work in scratch.
+#Sadly, it appears scratch only gets an output from the actual mouseclick/device, thus I couldn't achive the end goal.
